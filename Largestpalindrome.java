58db0c465868b5df325375dcdcbad16a3fdb4ce5
dhanavichu <90851378+dhanavichu@users.noreply.github.com>
2021-12-01 19:49:28 +0530

    Add files via upload

---
+++ Largestpalindrome.java
@@ -0,0 +1,42 @@
+package com.lucky;
+
+public class Largestpalindrome {
+	static boolean palindrome(int N)
+	   {
+	       int temp = 0, backup = N, b=0;
+	       while(N > 0)
+	       {
+	    	   b= N%10;
+	           temp = temp * 10 +b;
+	           N = (int)Math.floor(N / 10);
+	       }
+	       if(temp == backup)
+	           return true;
+	       return false;
+	   }
+	   static int largest_palindrome()
+	   {
+	       int largest = 0;
+	       for(int i = 100; i <= 999; i++)
+	       {
+	           for(int j=100; j<=999; j++)
+	           {
+	               int N = i * j;
+	               if(palindrome(N) && N > largest)
+	                   largest = N;
+	           }
+	       }
+	       return largest;
+	   }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int answer = largest_palindrome();
+	   	System.out.println(answer);
+			
+			
+	}
+
+}
+
+
